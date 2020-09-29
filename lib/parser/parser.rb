f5c23e4e24ac8f983dc4136595301e1357b87e4a
K.Nechepurenko <kot@promarket.ru>
2020-09-29 12:27:18 +0000

    Parser specs

---
+++ lib/parser/parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+##
+# The Parser parse a file and generates the lines
+class Parser
+  def self.act(file_path)
+  end
+end
---
+++ lib/parser/uniq_parser.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+##
+# The UniqParser parse a file and generates the lines for the uniq paths
+class UniqParser < Parser
+end
