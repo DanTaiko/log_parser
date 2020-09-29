237091a41268a0ad246e866df975d9126c13ce0b
K.Nechepurenko <kot@promarket.ru>
2020-09-29 12:40:05 +0000

    Parser v0.0.1

--- lib/parser/uniq_parser.rb
+++ lib/parser/uniq_parser.rb
@@ -3,4 +3,6 @@
 ##
 # The UniqParser parse a file and generates the lines for the uniq paths
 class UniqParser < Parser
+  BUILDER = UniqPrintLine
+  PATH_REGEXP = %r{\A(\/[a-z\_]+(\/\d+)?)\s}.freeze
 end
