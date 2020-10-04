48fac754ed2c3327bfa9198d9fa141e8e460bd19
K.Nechepurenko <kot@promarket.ru>
2020-10-04 09:10:51 +0000

    Line refactoring spec

--- spec/unit_tests/models/line_spec.rb
+++ spec/unit_tests/models/line_spec.rb
@@ -2,36 +2,44 @@
 
 require 'spec_helper'
 
-describe 'Line' do
+describe 'Line initialization' do
   let(:symbol) { 'symbol' }
+  let(:amount) { 10 }
+  let(:title) { 'title' }
 
-  context 'initialization' do
-    it 'with symbol' do
-      line = Line.new(symbol: symbol)
-      expect(line.symbol).to eq symbol
-      expect(line.amount).to eq 1
-    end
-
-    it 'with symbol and amount' do
-      line = Line.new(symbol: symbol, amount: 10)
-      expect(line.symbol).to eq symbol
-      expect(line.amount).to eq 10
-    end
-
-    it 'fails without symbol' do
-      expect { Line.new }.to raise_error(ArgumentError)
-    end
+  it 'with symbol' do
+    line = Line.new(symbol: symbol)
+    expect(line.symbol).to eq symbol
+    expect(line.amount).to eq 1
   end
 
-  context 'to_s' do
-    let(:line) { Line.new(symbol: symbol) }
+  it 'with symbol and amount' do
+    line = Line.new(symbol: symbol, amount: amount)
+    expect(line.symbol).to eq symbol
+    expect(line.amount).to eq amount
+  end
 
-    it 'gives a meaningful string' do
-      expect(line.to_s).to eq "#{symbol} 1"
-    end
+  it 'with symbol, amount and title' do
+    line = Line.new(symbol: symbol, amount: amount, title: title)
+    expect(line.symbol).to eq symbol
+    expect(line.amount).to eq amount
+    expect(line.title).to eq title
+  end
 
-    it 'with postfix' do
-      expect(line.to_s(postfix: 'postfix')).to eq "#{symbol} 1 postfix"
-    end
+  it 'fails without symbol' do
+    expect { Line.new }.to raise_error(ArgumentError)
+  end
+end
+
+describe 'Line#to_s' do
+  let(:title) { 'title' }
+  let(:line) { Line.new(symbol: 'symbol', title: title) }
+
+  it 'gives a meaningful string' do
+    expect(line.to_s).to eq "#{title} 1"
+  end
+
+  it 'with postfix' do
+    expect(line.to_s(postfix: 'postfix')).to eq "#{title} 1 postfix"
   end
 end
