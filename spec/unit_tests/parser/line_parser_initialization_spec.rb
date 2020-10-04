3c6575ab9fd74fd3dfc467340be17c582ce2d1a8
K.Nechepurenko <kot@promarket.ru>
2020-10-04 09:25:32 +0000

    LineParser refactoring spec

--- spec/unit_tests/parser/line_parser_initialization_spec.rb
+++ spec/unit_tests/parser/line_parser_initialization_spec.rb
@@ -2,23 +2,37 @@
 
 require 'spec_helper'
 
-describe 'LineParser' do
+describe 'LineParser initialization' do
   before(:all) do
-    @file_line = 'symbol 111.222.333.444'
+    # file_line: 'symbol title'
     @symbol_regex = /\A(?<symbol>\w+)/
+    @title_regex = /(?<title>\w+)\z/
   end
 
-  let(:parser) { LineParser.new(symbol_regex: @symbol_regex) }
+  let(:parser) { LineParser.new(symbol_regex: @symbol_regex, title_regex: @title_regex) }
 
-  context 'initialization' do
-    it 'keeping the symbol_regex value' do
-      expect(parser.symbol_regex).to eq @symbol_regex
-    end
+  it 'keeping the symbol_regex and title_regex values' do
+    expect(parser.symbol_regex).to eq @symbol_regex
+    expect(parser.title_regex).to eq @title_regex
+  end
 
-    it 'fails without symbol_regex' do
-      expect { LineParser.new }.to raise_error(ArgumentError)
-    end
+  it 'fails without symbol_regex' do
+    expect { LineParser.new }.to raise_error(ArgumentError)
+  end
 
+  it 'pass without title_regex' do
+    expect { LineParser.new(symbol_regex: @symbol_regex) }.not_to raise_error
+  end
+end
+
+describe 'LineParser validation' do
+  before(:all) do
+    # file_line: 'symbol title'
+    @symbol_regex = /\A(?<symbol>\w+)/
+    @title_regex = /(?<title>\w+)\z/
+  end
+
+  context 'for symbol_regex' do
     it 'fails with symbol_regex that is missing a "symbol" named_captures' do
       regex = /\w+/
       expect { LineParser.new(symbol_regex: regex) }.to raise_error(ArgumentError)
@@ -26,6 +40,17 @@ describe 'LineParser' do
 
     it 'fails with a bad symbol_regex' do
       expect { LineParser.new(symbol_regex: 'symbol_regex') }.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'for title_regex' do
+    it 'fails with title_regex that is missing a "title" named_captures' do
+      regex = /\w+/
+      expect { LineParser.new(symbol_regex: @symbol_regex, title_regex: regex) }.to raise_error(ArgumentError)
+    end
+
+    it 'fails with a bad title_regex' do
+      expect { LineParser.new(symbol_regex: @symbol_regex, title_regex: 'title_regex') }.to raise_error(ArgumentError)
     end
   end
 end
--- spec/unit_tests/parser/line_parser_methods_spec.rb
+++ spec/unit_tests/parser/line_parser_methods_spec.rb
@@ -2,29 +2,69 @@
 
 require 'spec_helper'
 
-describe 'LineParser' do
+describe 'LineParser#find_symbol' do
   before(:all) do
-    @file_line = 'symbol 111.222.333.444'
+    @file_line = 'symbol title'
     @symbol_regex = /\A(?<symbol>\w+)/
   end
 
   let(:parser) { LineParser.new(symbol_regex: @symbol_regex) }
 
-  context 'find_symbol' do
-    it 'fails without file_line' do
-      expect { parser.find_symbol }.to raise_error(ArgumentError)
-    end
+  it 'fails without file_line' do
+    expect { parser.find_symbol }.to raise_error(ArgumentError)
+  end
 
-    it 'fails with a bad file_line' do
-      expect { parser.find_symbol(file_line: 123) }.to raise_error(ArgumentError)
-    end
+  it 'fails with a bad file_line' do
+    expect { parser.find_symbol(file_line: 123) }.to raise_error(ArgumentError)
+  end
 
-    it 'fails for the line without symbol' do
-      expect { parser.find_symbol(file_line: '') }.to raise_error(ArgumentError)
-    end
+  it 'fails for the line without symbol' do
+    expect { parser.find_symbol(file_line: '') }.to raise_error(ArgumentError)
+  end
 
-    it 'return the founded sysbol' do
-      expect(parser.find_symbol(file_line: @file_line)).to eq 'symbol'
-    end
+  it 'return the founded sysbol' do
+    expect(parser.find_symbol(file_line: @file_line)).to eq 'symbol'
+  end
+end
+
+describe 'LineParser#find_title' do
+  before(:all) do
+    @file_line = 'symbol title'
+    @symbol_regex = /\A(?<symbol>\w+)/
+    @title_regex = /(?<title>\w+)\z/
+  end
+
+  let(:parser) { LineParser.new(symbol_regex: @symbol_regex, title_regex: @title_regex) }
+
+  it 'fails without file_line' do
+    expect { parser.find_title }.to raise_error(ArgumentError)
+  end
+
+  it 'fails with a bad file_line' do
+    expect { parser.find_title(file_line: 123) }.to raise_error(ArgumentError)
+  end
+
+  it 'fails for the line without title' do
+    expect { parser.find_title(file_line: '') }.to raise_error(ArgumentError)
+  end
+
+  it 'return the founded title' do
+    expect(parser.find_symbol(file_line: @file_line)).to eq 'title'
+  end
+end
+
+describe 'LineParser#find_symbol_and_title' do
+  before(:all) do
+    @file_line = 'symbol title'
+    @symbol_regex = /\A(?<symbol>\w+)/
+    @title_regex = /(?<title>\w+)\z/
+  end
+
+  let(:parser) { LineParser.new(symbol_regex: @symbol_regex, title_regex: @title_regex) }
+  
+  it 'calls #find_symbol and #find_title' do
+    expect(parser).to receive(:find_symbol).with(@file_line).once.and_return('symbol')
+    expect(parser).to receive(:find_title).with(@file_line).once.and_return('title')
+    expect(director.find_symbol_and_title).to eq ['symbol', 'title']
   end
 end
