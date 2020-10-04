0754bfeabcdcca577a389d0b8d781f0aa7570a9d
K.Nechepurenko <kot@promarket.ru>
2020-10-04 09:27:22 +0000

    SymbolRegexValidator refactoring spec

--- spec/unit_tests/parser/line_parser_methods_spec.rb
+++ spec/unit_tests/parser/line_parser_methods_spec.rb
@@ -61,10 +61,10 @@ describe 'LineParser#find_symbol_and_title' do
   end
 
   let(:parser) { LineParser.new(symbol_regex: @symbol_regex, title_regex: @title_regex) }
-  
+
   it 'calls #find_symbol and #find_title' do
     expect(parser).to receive(:find_symbol).with(@file_line).once.and_return('symbol')
     expect(parser).to receive(:find_title).with(@file_line).once.and_return('title')
-    expect(director.find_symbol_and_title).to eq ['symbol', 'title']
+    expect(director.find_symbol_and_title).to eq %w[symbol title]
   end
 end
--- spec/unit_tests/validators/symbol_regex_validator_spec.rb
+++ spec/unit_tests/validators/symbol_regex_validator_spec.rb
@@ -2,29 +2,57 @@
 
 require 'spec_helper'
 
-describe 'SymbolRegexValidator' do
+describe 'SymbolRegexValidator initialization' do
   before(:all) do
+    @symbol_attr = 'symbol'
     @symbol_regex = /\A(?<symbol>\w+)/
   end
 
-  let(:validator) { SymbolRegexValidator.new(symbol_regex: @symbol_regex) }
+  let(:validator) { SymbolRegexValidator.new(symbol_regex: @symbol_regex, symbol_attr: symbol_attr) }
 
-  context 'initialization' do
-    it 'keeping the symbol_regex value' do
-      expect(validator.symbol_regex).to eq @symbol_regex
-    end
+  it 'keeping the symbol_regex value' do
+    expect(validator.symbol_regex).to eq @symbol_regex
+  end
 
-    it 'fails without symbol_regex' do
-      expect { SymbolRegexValidator.new }.to raise_error(ArgumentError)
-    end
+  it 'keeping the symbol_attr value' do
+    expect(validator.symbol_attr).to eq @symbol_attr
+  end
 
-    it 'fails with symbol_regex that is missing a "symbol" named_captures' do
-      regex = /\w+/
-      expect(SymbolRegexValidator.new(symbol_regex: regex).valid?).to be_falsey
-    end
+  it 'fails without symbol_regex' do
+    expect { SymbolRegexValidator.new }.to raise_error(ArgumentError)
+  end
 
-    it 'fails with a bad symbol_regex' do
-      expect(SymbolRegexValidator.new(symbol_regex: 'symbol_regex').valid?).to be_falsey
-    end
+  it 'pass without symbol_attr' do
+    expect { SymbolRegexValidator.new(symbol_regex: @symbol_regex) }.not_to raise_error
+  end
+end
+
+describe 'SymbolRegexValidator#valid?' do
+  before(:all) do
+    @symbol_attr = 'symbol'
+    @symbol_regex = /\A(?<symbol>\w+)/
+  end
+
+  let(:validator) { SymbolRegexValidator.new(symbol_regex: @symbol_regex, symbol_attr: symbol_attr) }
+
+  it 'pass for valid regex' do
+    expect(validator.valid?).to be_falsey
+  end
+
+  it 'fails with symbol_regex that is missing a "symbol" named_captures' do
+    regex = /\w+/
+    validator = SymbolRegexValidator.new(symbol_regex: regex)
+    expect(validator.valid?).to be_falsey
+  end
+
+  it 'fails with a bad symbol_regex' do
+    regex = 'symbol_regex'
+    validator = SymbolRegexValidator.new(symbol_regex: regex)
+    expect(validator.valid?).to be_falsey
+  end
+
+  it 'fails with symbol_regex that is missing a "title" named_captures' do
+    validator = SymbolRegexValidator.new(symbol_regex: @symbol_regex, symbol_attr: 'title')
+    expect(validator.valid?).to be_falsey
   end
 end
