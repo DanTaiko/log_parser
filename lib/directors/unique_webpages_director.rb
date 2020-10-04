2458015a0d34c973e1a618befd60b74dfd4c0cc3
K.Nechepurenko <kot@promarket.ru>
2020-10-04 09:59:39 +0000

    Integration tests and Code cleanup

--- lib/directors/unique_webpages_director.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-##
-# The UniqueWebpagesDirector runs the show for the 'unique_webpages_rating' command
-class UniqueWebpagesDirector < WebpagesDirector
-  SYMBOL_REGEX = %r{\A(?<symbol>\/[a-z\_]+(\/\d+)?)\s}.freeze
-  PRINT_DETAILS = { header: 'unique_webpages_rating', line_postfix: 'unique views' }.freeze
-end
--- spec/integration/intergation_spec.rb
+++ spec/integration/intergation_spec.rb
@@ -4,7 +4,14 @@ require 'spec_helper'
 require_relative '../../lib/parser_cli.rb'
 
 describe 'Integration' do
-  context 'for webpages_rating' do
+  context 'for analyze' do
+    it 'for analyze' do
+      output = File.read('spec/fixtures/files/analyze.out')
+      expect { system %(bin/parser analyze spec/fixtures/files/webserver.log) }
+        .to output(output)
+        .to_stdout_from_any_process
+    end
+
     it 'for webpages_rating' do
       output = File.read('spec/fixtures/files/webpages_rating.out')
       expect { system %(bin/parser webpages_rating spec/fixtures/files/webserver.log) }
