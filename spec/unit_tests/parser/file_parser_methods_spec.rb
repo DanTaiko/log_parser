caafa361af562a1d6a22a2278906efeed499e132
K.Nechepurenko <kot@promarket.ru>
2020-10-04 09:35:49 +0000

    FileParser refactoring specs

--- spec/unit_tests/parser/file_parser_methods_spec.rb
+++ spec/unit_tests/parser/file_parser_methods_spec.rb
@@ -6,19 +6,21 @@ describe 'FileParser' do
   before(:all) do
     @file_path = 'spec/fixtures/files/webserver.log'
     @file_data = File.read(@file_path).split("\n").map { |line| line.split[0] }
-    @symbol_regex = %r{\A(?<symbol>(\/[a-z\_]+)(\/\d+)?)\s}.freeze
-    @collector = []
-    def @collector.add(symbol)
-      self << symbol
-    end
+    @symbol_regex = %r{\A(?<symbol>[\w\.\/]+)\s}.freeze
+    @title_regex = /\s(?<title>[\w\.]+)$/.freeze
+    @collector = Collector.new(builder: Line)
   end
 
-  let(:parser) { FileParser.build(file_path: @file_path, collector: @collector, symbol_regex: @symbol_regex) }
+  let(:parser) do
+    FileParser.build(file_path: @file_path,
+                     collector: @collector,
+                     symbol_regex: [@symbol_regex, @title_regex])
+  end
 
   context 'parse' do
     it 'collect symbols in collection.' do
       parser.parse
-      expect(@collector).to eq @file_data
+      expect(@collector.members.map(&:symbol)).to eq @file_data
     end
     it 'call the line_parser for each file line.' do
       expect(parser.line_parser).to receive(:find_symbol).exactly(@file_data.count).time
